f760589bc3bd32dcff2c1b310dbaa73058e4ad6c
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-08-31 11:43:14 +0300

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char text = 'a';
+while(i < 10){
+
+	while (text <= 'z')
+	{
+		_putchar(text);
+		text++;
+	}
+	_putchar('\n');
+}
+}
