f22f5c5072b911913ce643dd740c0db2840fe737
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-08-31 14:36:19 +0300

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+void print_to_98(int n){
+while(n < 98){
+if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}
+}
