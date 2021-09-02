d6e32ba8e5ff150be21573a51388c7c190eda96b
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-09-02 17:20:50 +0300

    Task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,15 @@
-int _isupper(int c){
-if(c >= 64 && c <= 91){
-return (1)
-} else {
-return (0)
+#include "holberton.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
-}
+
