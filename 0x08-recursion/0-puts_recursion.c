0960a954d4c910727f623178ad9f48a9a5bd5c0e
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-09-15 20:27:16 +0300

    small changes

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,12 @@
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}void _puts_recursion(char *s){
 printf("%s\n", *s);
 }
