e672950f2be4aaa5b803f99c860fb007fb01ebd4
Dennis Kiplang'at <denniskiplangat.dk@gmail.com>
2021-10-15 01:57:35 +0300

    Initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,17 @@
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal;
+	unsigned int i;
+
+	for (decimal = 0, i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else if (b[i] == '0')
+			decimal <<= 1;
+		else if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	return (decimal);
+}
