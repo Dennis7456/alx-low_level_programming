79575c8ef2988086e3087e846bf103ffe225fe8c
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-10-19 02:50:25 +0300

    small changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,6 @@
 #include "main.h"
 
-/**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
- */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file, rd, wr;
