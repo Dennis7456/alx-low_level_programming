f35683c317e902f6be2d3dca50ecd602d800a70e
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-10-19 02:30:59 +0300

    testing

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,39 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "holberton.h"
 
 /**
- * read_textfile - prints text from a file
- *
- * @filename: name of the file
- * @letters: number of characters to read
- *
- * Return: actual number of letters read, 0 if end of file
+ * read_textfile - Entry Point
+ * @filename: file name
+ * @letters: size
+ * Return: 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file;
-	int length, wrotechars;
+	int file, rd, wr;
 	char *buf;
 
-	if (filename == NULL || letters == 0)
-		return (0);
-	buf = malloc(sizeof(char) * (letters));
-	if (buf == NULL)
+	if (filename == NULL)
 		return (0);
 
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
-	{
-		free(buf);
 		return (0);
-	}
-	length = read(file, buf, letters);
-	if (length == -1)
-	{
-		free(buf);
-		close(file);
+
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
 		return (0);
-	}
 
-	wrotechars = write(STDOUT_FILENO, buf, length);
+	rd = read(file, buf, letters);
+	if (rd == -1)
+		return (0);
 
-	free(buf);
+	buf[letters] = '\0';
+
+	wr = write(1, buf, rd);
+	if (wr == -1)
+		return (0);
+
 	close(file);
-	if (wrotechars != length)
-		return (0);
-	return (length);
+	free(buf);
+	return (wr);
 }
