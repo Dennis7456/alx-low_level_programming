7e90e445d84f68a7d82af2e9290d4e93c70ce13d
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-10-19 02:07:39 +0300

    a different approach

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,46 @@
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ * read_textfile - prints text from a file
+ *
+ * @filename: name of the file
+ * @letters: number of characters to read
+ *
+ * Return: actual number of letters read, 0 if end of file
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
+	int file;
+	int length, wrotechars;
 	char *buf;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters + 1);
+	buf = malloc(sizeof(char) * (letters));
 	if (buf == NULL)
 		return (0);
 
-	rd = read(file, buf, letters);
-	if (rd == -1)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(buf);
 		return (0);
-
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
+	}
+	length = read(file, buf, letters);
+	if (length == -1)
+	{
+		free(buf);
+		close(file);
 		return (0);
+	}
 
-	close(file);
+	wrotechars = write(STDOUT_FILENO, buf, length);
+
 	free(buf);
-	return (wr);
+	close(file);
+	if (wrotechars != length)
+		return (0);
+	return (length);
 }
