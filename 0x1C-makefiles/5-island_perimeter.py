94b62ffa800a3d81084617e38ff80fa87ffb8f3a
Dennis7456 <denniskiplangat.dk@gmail.com>
2022-02-17 00:45:34 +0300

    Task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/Python3
+
+"""Fucntion to return perimeter of island described in the grid"""
+def island_perimeter(grid):
+	count = 0
+	height = len(grid)-1
+	width = len(grid[0]) -1
+	for i, arr in enumerate(grid):
+		for j, el in enumerate(arr):
+			if el == 1:
+				if i == 0 or grid[i - 1][j] != 1:
+					count += 1
+				if j == 0 or grid[i][j - 1] != 1:
+					count += 1
+				if j == width or grid[i][j + 1] != 1:
+					count += 1
+				if i == height or grid[i + 1][j] != 1:
+					count += 1
+	return count
